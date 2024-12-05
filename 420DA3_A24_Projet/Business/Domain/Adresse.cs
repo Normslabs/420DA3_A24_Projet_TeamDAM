b47358c843c80eb1ca43c5490a8e544a61df4566
Gourar Moataz <gourar.moataz@lcieducation.net>
2024-12-05 14:10:54 -0500

    Création de la classe Adresse

---
+++ 420DA3_A24_Projet/Business/Domain/Adresse.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_A24_Projet.Business.Domain;
+
+/// <summary>
+/// Classe représentant une adresse associée à un client ou à un entrepôt.
+/// </summary>
+public class Adresse {
+    /// <summary>
+    /// Longueur maximum des champs de texte, en caractères.
+    /// </summary>
+    public const int TYPE_MAX_LENGTH = 32;
+    public const int DESTINATAIRE_MAX_LENGTH = 128;
+    public const int NUMERO_CIVIQUE_MAX_LENGTH = 16;
+    public const int RUE_MAX_LENGTH = 128;
+    public const int VILLE_MAX_LENGTH = 64;
+    public const int PROVINCE_MAX_LENGTH = 64;
+    public const int PAYS_MAX_LENGTH = 64;
+    public const int CODE_POSTAL_MAX_LENGTH = 16;
+
+    // Backing fields
+    private int id;
+    private string typeAdresse = null!;
+    private string destinataire = null!;
+    private string numeroCivique = null!;
+    private string rue = null!;
+
+    #region Propriétés de données
+
+    public int Id {
+        get { return this.id; }
+        set {
+            if (!ValiderId(value)) {
+                throw new ArgumentOutOfRangeException("Id", "L'Id doit être supérieur ou égal à 0.");
+            }
+            this.id = value;
+        }
+    }
+
+    public string TypeAdresse {
+        get { return this.typeAdresse; }
+        set {
+            if (!ValiderTypeAdresse(value)) {
+                throw new ArgumentOutOfRangeException("TypeAdresse", $"Le type d'adresse doit contenir au maximum {TYPE_MAX_LENGTH} caractères.");
+            }
+            this.typeAdresse = value;
+        }
+    }
+
+    public string Destinataire {
+        get { return this.destinataire; }
+        set {
+            if (!ValiderDestinataire(value)) {
+                throw new ArgumentOutOfRangeException("Destinataire", $"Le destinataire doit contenir au maximum {DESTINATAIRE_MAX_LENGTH} caractères.");
+            }
+            this.destinataire = value;
+        }
+    }
+
+    public string NumeroCivique {
+        get { return this.numeroCivique; }
+        set {
+            if (!ValiderNumeroCivique(value)) {
+                throw new ArgumentOutOfRangeException("NumeroCivique", $"Le numéro civique doit contenir au maximum {NUMERO_CIVIQUE_MAX_LENGTH} caractères.");
+            }
+            this.numeroCivique = value;
+        }
+    }
+
+    public string Rue {
+        get { return this.rue; }
+        set {
+            if (!ValiderRue(value)) {
+                throw new ArgumentOutOfRangeException("Rue", $"La rue doit contenir au maximum {RUE_MAX_LENGTH} caractères.");
+            }
+            this.rue = value;
+        }
+    }
+
+    public string Ville { get; set; } = null!;
+    public string Province { get; set; } = null!;
+    public string Pays { get; set; } = null!;
+    public string CodePostal { get; set; } = null!;
+    public DateTime DateCreation { get; set; }
+    public DateTime? DateModification { get; set; }
+    public DateTime? DateSuppression { get; set; }
+
+    #endregion
+
+    #region Propriétés de navigation
+
+    /// <summary>
+    /// Client ou entrepôt associé à l'adresse.
+    /// </summary>
+    public virtual Client? Client { get; set; }
+
+    #endregion
+
+    #region Constructeurs
+
+    /// <summary>
+    /// Constructeur principal.
+    /// </summary>
+    /// <param name="typeAdresse">Le type d'adresse (entrepôt ou destinataire).</param>
+    /// <param name="destinataire">Le nom du destinataire ou du bâtiment.</param>
+    /// <param name="numeroCivique">Le numéro civique.</param>
+    /// <param name="rue">Le nom de la rue.</param>
+    /// <param name="ville">La ville.</param>
+    /// <param name="province">La province.</param>
+    /// <param name="pays">Le pays.</param>
+    /// <param name="codePostal">Le code postal.</param>
+    public Adresse(string typeAdresse, string destinataire, string numeroCivique, string rue, string ville, string province, string pays, string codePostal) {
+        this.TypeAdresse = typeAdresse;
+        this.Destinataire = destinataire;
+        this.NumeroCivique = numeroCivique;
+        this.Rue = rue;
+        this.Ville = ville;
+        this.Province = province;
+        this.Pays = pays;
+        this.CodePostal = codePostal;
+        this.DateCreation = DateTime.Now;
+    }
+
+    /// <summary>
+    /// Constructeur orienté Entity Framework.
+    /// </summary>
+    protected Adresse(int id, string typeAdresse, string destinataire, string numeroCivique, string rue, string ville, string province, string pays, string codePostal, DateTime dateCreation, DateTime? dateModification, DateTime? dateSuppression)
+        : this(typeAdresse, destinataire, numeroCivique, rue, ville, province, pays, codePostal) {
+        this.Id = id;
+        this.DateCreation = dateCreation;
+        this.DateModification = dateModification;
+        this.DateSuppression = dateSuppression;
+    }
+
+    #endregion
+
+    #region Méthodes
+
+    public static bool ValiderId(int id) {
+        return id >= 0;
+    }
+
+    public static bool ValiderTypeAdresse(string typeAdresse) {
+        return typeAdresse.Length <= TYPE_MAX_LENGTH;
+    }
+
+    public static bool ValiderDestinataire(string destinataire) {
+        return destinataire.Length <= DESTINATAIRE_MAX_LENGTH;
+    }
+
+    public static bool ValiderNumeroCivique(string numeroCivique) {
+        return numeroCivique.Length <= NUMERO_CIVIQUE_MAX_LENGTH;
+    }
+
+    public static bool ValiderRue(string rue) {
+        return rue.Length <= RUE_MAX_LENGTH;
+    }
+
+    #endregion
+}
+
---
+++ 420DA3_A24_Projet/Business/Domain/Client.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+namespace _420DA3_A24_Projet.Business.Domain;
+
+/// <summary>
+/// Classe représentant un client.
+/// </summary>
+public class Client {
+    /// <summary>
+    /// Longueur maximum des champs de texte, en caractères.
+    /// </summary>
+    public const int NOM_COMPAGNIE_MAX_LENGTH = 128;
+    public const int NOM_PERSONNE_MAX_LENGTH = 64;
+    public const int PRENOM_PERSONNE_MAX_LENGTH = 64;
+    public const int COURRIEL_MAX_LENGTH = 128;
+    public const int TELEPHONE_MAX_LENGTH = 20;
+
+    // Backing fields
+    private int id;
+    private string nomCompagnie = null!;
+    private string nomContact = null!;
+    private string prenomContact = null!;
+    private string courrielContact = null!;
+    private string telephoneContact = null!;
+
+    #region Propriétés de données
+
+    public int Id {
+        get { return this.id; }
+        set {
+            if (!ValiderId(value)) {
+                throw new ArgumentOutOfRangeException("Id", "L'Id doit être supérieur ou égal à 0.");
+            }
+            this.id = value;
+        }
+    }
+
+    public string NomCompagnie {
+        get { return this.nomCompagnie; }
+        set {
+            if (!ValiderNomCompagnie(value)) {
+                throw new ArgumentOutOfRangeException("NomCompagnie", $"Le nom de la compagnie doit contenir au maximum {NOM_COMPAGNIE_MAX_LENGTH} caractères.");
+            }
+            this.nomCompagnie = value;
+        }
+    }
+
+    public string NomContact {
+        get { return this.nomContact; }
+        set {
+            if (!ValiderNomContact(value)) {
+                throw new ArgumentOutOfRangeException("NomContact", $"Le nom de la personne-contact doit contenir au maximum {NOM_PERSONNE_MAX_LENGTH} caractères.");
+            }
+            this.nomContact = value;
+        }
+    }
+
+    public string PrenomContact {
+        get { return this.prenomContact; }
+        set {
+            if (!ValiderPrenomContact(value)) {
+                throw new ArgumentOutOfRangeException("PrenomContact", $"Le prénom de la personne-contact doit contenir au maximum {PRENOM_PERSONNE_MAX_LENGTH} caractères.");
+            }
+            this.prenomContact = value;
+        }
+    }
+
+    public string CourrielContact {
+        get { return this.courrielContact; }
+        set {
+            if (!ValiderCourrielContact(value)) {
+                throw new ArgumentOutOfRangeException("CourrielContact", $"Le courriel de la personne-contact doit contenir au maximum {COURRIEL_MAX_LENGTH} caractères.");
+            }
+            this.courrielContact = value;
+        }
+    }
+
+    public string TelephoneContact {
+        get { return this.telephoneContact; }
+        set {
+            if (!ValiderTelephoneContact(value)) {
+                throw new ArgumentOutOfRangeException("TelephoneContact", $"Le téléphone de la personne-contact doit contenir au maximum {TELEPHONE_MAX_LENGTH} caractères.");
+            }
+            this.telephoneContact = value;
+        }
+    }
+
+    public DateTime DateCreation { get; set; }
+    public DateTime? DateModification { get; set; }
+    public DateTime? DateSuppression { get; set; }
+
+    #endregion
+
+    #region Propriétés de navigation
+
+    /// <summary>
+    /// Entrepôt attitré au client.
+    /// </summary>
+    public virtual Entrepot? EntrepotAttitre { get; set; }
+
+    #endregion
+
+    #region Constructeurs
+
+    /// <summary>
+    /// Constructeur principal.
+    /// </summary>
+    /// <param name="nomCompagnie">Nom de la compagnie.</param>
+    /// <param name="nomContact">Nom de la personne-contact.</param>
+    /// <param name="prenomContact">Prénom de la personne-contact.</param>
+    /// <param name="courrielContact">Courriel de la personne-contact.</param>
+    /// <param name="telephoneContact">Téléphone de la personne-contact.</param>
+    public Client(string nomCompagnie, string nomContact, string prenomContact, string courrielContact, string telephoneContact) {
+        this.NomCompagnie = nomCompagnie;
+        this.NomContact = nomContact;
+        this.PrenomContact = prenomContact;
+        this.CourrielContact = courrielContact;
+        this.TelephoneContact = telephoneContact;
+        this.DateCreation = DateTime.Now;
+    }
+
+    /// <summary>
+    /// Constructeur orienté Entity Framework.
+    /// </summary>
+    protected Client(int id, string nomCompagnie, string nomContact, string prenomContact, string courrielContact, string telephoneContact, DateTime dateCreation, DateTime? dateModification, DateTime? dateSuppression)
+        : this(nomCompagnie, nomContact, prenomContact, courrielContact, telephoneContact) {
+        this.Id = id;
+        this.DateCreation = dateCreation;
+        this.DateModification = dateModification;
+        this.DateSuppression = dateSuppression;
+    }
+
+    #endregion
+
+    #region Méthodes
+
+    public static bool ValiderId(int id) {
+        return id >= 0;
+    }
+
+    public static bool ValiderNomCompagnie(string nomCompagnie) {
+        return nomCompagnie.Length <= NOM_COMPAGNIE_MAX_LENGTH;
+    }
+
+    public static bool ValiderNomContact(string nomContact) {
+        return nomContact.Length <= NOM_PERSONNE_MAX_LENGTH;
+    }
+
+    public static bool ValiderPrenomContact(string prenomContact) {
+        return prenomContact.Length <= PRENOM_PERSONNE_MAX_LENGTH;
+    }
+
+    public static bool ValiderCourrielContact(string courrielContact) {
+        return courrielContact.Length <= COURRIEL_MAX_LENGTH;
+    }
+
+    public static bool ValiderTelephoneContact(string telephoneContact) {
+        return telephoneContact.Length <= TELEPHONE_MAX_LENGTH;
+    }
+
+    #endregion
+}
