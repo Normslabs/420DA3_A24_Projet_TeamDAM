4c5059041be35d8da917a57ed9264ccf781b3f31
Gourar Moataz <gourar.moataz@lcieducation.net>
2024-12-13 22:08:55 -0500

    ClientView et AdresseView

---
+++ 420DA3_A24_Projet/Presentation/Views/AdresseView.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace _420DA3_A24_Projet.Presentation.Views;
+
+partial class AdresseView {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+        if (disposing && (components != null)) {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+        this.panel1 = new Panel();
+        this.panel2 = new Panel();
+        this.panel3 = new Panel();
+        this.tableLayoutPanel1 = new TableLayoutPanel();
+        this.panel4 = new Panel();
+        this.dateTimePicker3 = new DateTimePicker();
+        this.dateTimePicker2 = new DateTimePicker();
+        this.dateTimePicker1 = new DateTimePicker();
+        this.textBox6 = new TextBox();
+        this.textBox5 = new TextBox();
+        this.textBox4 = new TextBox();
+        this.textBox3 = new TextBox();
+        this.textBox2 = new TextBox();
+        this.textBox1 = new TextBox();
+        this.numericUpDown1 = new NumericUpDown();
+        this.label21 = new Label();
+        this.label20 = new Label();
+        this.label19 = new Label();
+        this.label18 = new Label();
+        this.label17 = new Label();
+        this.label16 = new Label();
+        this.label12 = new Label();
+        this.label13 = new Label();
+        this.label14 = new Label();
+        this.label15 = new Label();
+        this.panel1.SuspendLayout();
+        this.tableLayoutPanel1.SuspendLayout();
+        this.panel4.SuspendLayout();
+        ((System.ComponentModel.ISupportInitialize) this.numericUpDown1).BeginInit();
+        this.SuspendLayout();
+        // 
+        // panel1
+        // 
+        this.panel1.Controls.Add(this.panel2);
+        this.panel1.Dock = DockStyle.Top;
+        this.panel1.Location = new Point(0, 0);
+        this.panel1.Name = "panel1";
+        this.panel1.Size = new Size(800, 60);
+        this.panel1.TabIndex = 0;
+        // 
+        // panel2
+        // 
+        this.panel2.Dock = DockStyle.Top;
+        this.panel2.Location = new Point(0, 0);
+        this.panel2.Name = "panel2";
+        this.panel2.Size = new Size(800, 60);
+        this.panel2.TabIndex = 1;
+        // 
+        // panel3
+        // 
+        this.panel3.Dock = DockStyle.Bottom;
+        this.panel3.Location = new Point(0, 390);
+        this.panel3.Name = "panel3";
+        this.panel3.Size = new Size(800, 60);
+        this.panel3.TabIndex = 2;
+        // 
+        // tableLayoutPanel1
+        // 
+        this.tableLayoutPanel1.ColumnCount = 3;
+        this.tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 13.25F));
+        this.tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 73.25F));
+        this.tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 13.5F));
+        this.tableLayoutPanel1.Controls.Add(this.panel4, 1, 0);
+        this.tableLayoutPanel1.Dock = DockStyle.Fill;
+        this.tableLayoutPanel1.Location = new Point(0, 60);
+        this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+        this.tableLayoutPanel1.RowCount = 1;
+        this.tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+        this.tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+        this.tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+        this.tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+        this.tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+        this.tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+        this.tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+        this.tableLayoutPanel1.Size = new Size(800, 330);
+        this.tableLayoutPanel1.TabIndex = 3;
+        // 
+        // panel4
+        // 
+        this.panel4.Controls.Add(this.dateTimePicker3);
+        this.panel4.Controls.Add(this.dateTimePicker2);
+        this.panel4.Controls.Add(this.dateTimePicker1);
+        this.panel4.Controls.Add(this.textBox6);
+        this.panel4.Controls.Add(this.textBox5);
+        this.panel4.Controls.Add(this.textBox4);
+        this.panel4.Controls.Add(this.textBox3);
+        this.panel4.Controls.Add(this.textBox2);
+        this.panel4.Controls.Add(this.textBox1);
+        this.panel4.Controls.Add(this.numericUpDown1);
+        this.panel4.Controls.Add(this.label21);
+        this.panel4.Controls.Add(this.label20);
+        this.panel4.Controls.Add(this.label19);
+        this.panel4.Controls.Add(this.label18);
+        this.panel4.Controls.Add(this.label17);
+        this.panel4.Controls.Add(this.label16);
+        this.panel4.Controls.Add(this.label12);
+        this.panel4.Controls.Add(this.label13);
+        this.panel4.Controls.Add(this.label14);
+        this.panel4.Controls.Add(this.label15);
+        this.panel4.Dock = DockStyle.Fill;
+        this.panel4.Location = new Point(109, 3);
+        this.panel4.Name = "panel4";
+        this.panel4.Size = new Size(580, 324);
+        this.panel4.TabIndex = 0;
+        // 
+        // dateTimePicker3
+        // 
+        this.dateTimePicker3.Location = new Point(249, 302);
+        this.dateTimePicker3.Name = "dateTimePicker3";
+        this.dateTimePicker3.Size = new Size(294, 27);
+        this.dateTimePicker3.TabIndex = 39;
+        // 
+        // dateTimePicker2
+        // 
+        this.dateTimePicker2.Location = new Point(249, 271);
+        this.dateTimePicker2.Name = "dateTimePicker2";
+        this.dateTimePicker2.Size = new Size(294, 27);
+        this.dateTimePicker2.TabIndex = 38;
+        // 
+        // dateTimePicker1
+        // 
+        this.dateTimePicker1.Location = new Point(249, 241);
+        this.dateTimePicker1.Name = "dateTimePicker1";
+        this.dateTimePicker1.Size = new Size(294, 27);
+        this.dateTimePicker1.TabIndex = 37;
+        // 
+        // textBox6
+        // 
+        this.textBox6.Location = new Point(249, 208);
+        this.textBox6.Name = "textBox6";
+        this.textBox6.Size = new Size(294, 27);
+        this.textBox6.TabIndex = 36;
+        // 
+        // textBox5
+        // 
+        this.textBox5.Location = new Point(249, 174);
+        this.textBox5.Name = "textBox5";
+        this.textBox5.Size = new Size(294, 27);
+        this.textBox5.TabIndex = 35;
+        // 
+        // textBox4
+        // 
+        this.textBox4.Location = new Point(249, 137);
+        this.textBox4.Name = "textBox4";
+        this.textBox4.Size = new Size(294, 27);
+        this.textBox4.TabIndex = 34;
+        // 
+        // textBox3
+        // 
+        this.textBox3.Location = new Point(249, 98);
+        this.textBox3.Name = "textBox3";
+        this.textBox3.Size = new Size(294, 27);
+        this.textBox3.TabIndex = 33;
+        // 
+        // textBox2
+        // 
+        this.textBox2.Location = new Point(249, 61);
+        this.textBox2.Name = "textBox2";
+        this.textBox2.Size = new Size(294, 27);
+        this.textBox2.TabIndex = 32;
+        // 
+        // textBox1
+        // 
+        this.textBox1.Location = new Point(249, 29);
+        this.textBox1.Name = "textBox1";
+        this.textBox1.Size = new Size(294, 27);
+        this.textBox1.TabIndex = 31;
+        // 
+        // numericUpDown1
+        // 
+        this.numericUpDown1.Location = new Point(249, -4);
+        this.numericUpDown1.Name = "numericUpDown1";
+        this.numericUpDown1.Size = new Size(294, 27);
+        this.numericUpDown1.TabIndex = 30;
+        // 
+        // label21
+        // 
+        this.label21.AutoSize = true;
+        this.label21.Location = new Point(40, 307);
+        this.label21.Name = "label21";
+        this.label21.Size = new Size(149, 20);
+        this.label21.TabIndex = 29;
+        this.label21.Text = "Date de Suppression:";
+        // 
+        // label20
+        // 
+        this.label20.AutoSize = true;
+        this.label20.Location = new Point(40, 276);
+        this.label20.Name = "label20";
+        this.label20.Size = new Size(154, 20);
+        this.label20.TabIndex = 28;
+        this.label20.Text = "Date de Modification:";
+        // 
+        // label19
+        // 
+        this.label19.AutoSize = true;
+        this.label19.Location = new Point(40, 242);
+        this.label19.Name = "label19";
+        this.label19.Size = new Size(125, 20);
+        this.label19.TabIndex = 27;
+        this.label19.Text = "Date de Création:";
+        // 
+        // label18
+        // 
+        this.label18.AutoSize = true;
+        this.label18.Location = new Point(40, 208);
+        this.label18.Name = "label18";
+        this.label18.Size = new Size(86, 20);
+        this.label18.TabIndex = 26;
+        this.label18.Text = "CodePostal:";
+        // 
+        // label17
+        // 
+        this.label17.AutoSize = true;
+        this.label17.Location = new Point(38, 174);
+        this.label17.Name = "label17";
+        this.label17.Size = new Size(40, 20);
+        this.label17.TabIndex = 25;
+        this.label17.Text = "Pays:";
+        // 
+        // label16
+        // 
+        this.label16.AutoSize = true;
+        this.label16.Location = new Point(38, 140);
+        this.label16.Name = "label16";
+        this.label16.Size = new Size(134, 20);
+        this.label16.TabIndex = 24;
+        this.label16.Text = "Rue-Ville-Province:";
+        // 
+        // label12
+        // 
+        this.label12.AutoSize = true;
+        this.label12.Location = new Point(38, 101);
+        this.label12.Name = "label12";
+        this.label12.Size = new Size(93, 20);
+        this.label12.TabIndex = 23;
+        this.label12.Text = "NumCivique:";
+        // 
+        // label13
+        // 
+        this.label13.AutoSize = true;
+        this.label13.Location = new Point(38, 61);
+        this.label13.Name = "label13";
+        this.label13.Size = new Size(92, 20);
+        this.label13.TabIndex = 22;
+        this.label13.Text = "Destinataire:";
+        this.label13.Click += this.label13_Click;
+        // 
+        // label14
+        // 
+        this.label14.AutoSize = true;
+        this.label14.Location = new Point(38, 30);
+        this.label14.Name = "label14";
+        this.label14.Size = new Size(109, 20);
+        this.label14.TabIndex = 21;
+        this.label14.Text = "Type d'adresse:";
+        // 
+        // label15
+        // 
+        this.label15.AutoSize = true;
+        this.label15.Location = new Point(39, -2);
+        this.label15.Name = "label15";
+        this.label15.Size = new Size(27, 20);
+        this.label15.TabIndex = 20;
+        this.label15.Text = "ID:";
+        // 
+        // AdresseView
+        // 
+        this.AutoScaleDimensions = new SizeF(8F, 20F);
+        this.AutoScaleMode = AutoScaleMode.Font;
+        this.ClientSize = new Size(800, 450);
+        this.Controls.Add(this.tableLayoutPanel1);
+        this.Controls.Add(this.panel3);
+        this.Controls.Add(this.panel1);
+        this.Name = "AdresseView";
+        this.Text = "AdresseView";
+        this.panel1.ResumeLayout(false);
+        this.tableLayoutPanel1.ResumeLayout(false);
+        this.panel4.ResumeLayout(false);
+        this.panel4.PerformLayout();
+        ((System.ComponentModel.ISupportInitialize) this.numericUpDown1).EndInit();
+        this.ResumeLayout(false);
+    }
+
+    #endregion
+
+    private Panel panel1;
+    private Panel panel2;
+    private Panel panel3;
+    private TableLayoutPanel tableLayoutPanel1;
+    private Panel panel4;
+    private DateTimePicker dateTimePicker3;
+    private DateTimePicker dateTimePicker2;
+    private DateTimePicker dateTimePicker1;
+    private TextBox textBox6;
+    private TextBox textBox5;
+    private TextBox textBox4;
+    private TextBox textBox3;
+    private TextBox textBox2;
+    private TextBox textBox1;
+    private NumericUpDown numericUpDown1;
+    private Label label21;
+    private Label label20;
+    private Label label19;
+    private Label label18;
+    private Label label17;
+    private Label label16;
+    private Label label12;
+    private Label label13;
+    private Label label14;
+    private Label label15;
+}
---
+++ 420DA3_A24_Projet/Presentation/Views/ClientView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _420DA3_A24_Projet.Presentation.Views;
+public partial class ClientView : Form {
+    public ClientView() {
+        InitializeComponent();
+    }
+
+    private void panel6_Paint(object sender, PaintEventArgs e) {
+
+    }
+}
